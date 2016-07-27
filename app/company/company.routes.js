d00d9f2c2c25b2b390a4cf89e89551150e797405
peter.mezei <peter.mezei@slamby.com>
2016-07-27 09:23:20 +0200

    Subpages added: - support, community, serverrequest, company information, partners, contact

--- app/company/company.routes.js
+++ app/company/company.routes.js
@@ -1,6 +1,10 @@
 "use strict";
 var company_component_1 = require('./company.component');
+var contact_component_1 = require('./contact/contact.component');
+var partners_component_1 = require('./partners/partners.component');
 exports.CompanyRoutes = [
     { path: 'company', component: company_component_1.CompanyComponent },
+    { path: 'company/contact', component: contact_component_1.ContactComponent },
+    { path: 'company/partners', component: partners_component_1.PartnersComponent }
 ];
 //# sourceMappingURL=company.routes.js.map
--- app/company/company.routes.ts
+++ app/company/company.routes.ts
@@ -1,7 +1,11 @@
 import { RouterConfig } from '@angular/router';
 
 import { CompanyComponent } from './company.component';
+import { ContactComponent } from './contact/contact.component';
+import { PartnersComponent } from './partners/partners.component';
 
 export const CompanyRoutes: RouterConfig = [
   { path: 'company', component: CompanyComponent },
+  { path: 'company/contact', component: ContactComponent },
+  { path: 'company/partners', component: PartnersComponent }
 ];
